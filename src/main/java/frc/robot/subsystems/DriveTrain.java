018e5cf75b9f2f12de765facc9fc4662d97825f5
Collin Kawahara <ckawahara3@gmail.com>
2022-01-14 21:28:18 -0800

    basic tank drive system, probably doesn't work (forgot to save DriveTrain before last commit)

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -11,4 +11,22 @@ public class DriveTrain extends SubsystemBase {
     //not entirely sure about these two??
     private final SpeedController m_leftMotor = new SpeedController(new PWMSparkMax(0));
     private final SpeedController m_rightMotor = new SpeedController(new PWMSparkMax(1));
+
+    private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+
+    /**create a new drive train subsystem */
+    public DriveTrain() {
+        super();
+    }
+
+    public void drive(double left, double right) {
+        m_drive.tankDrive(left, right);
+    }
+
+      /** Call log method every loop. */
+  @Override
+  public void periodic() {
+    log();
+  }
 }
